ce504aacd371730542a7a2d8829bed917b0e3783
David Schneider <83253477+DavidSchneider47@users.noreply.github.com>
2025-03-18 12:19:35 -0400

    updating site to add a templates and index.html

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
-import os  # Import os to get environment variables
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Boston Transit!"
+    return render_template("index.html")  # Render HTML template
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Get PORT from Heroku, default to 5000 locally
-    app.run(host="0.0.0.0", port=port)  # Bind to all network interfaces
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from Heroku
+    app.run(host="0.0.0.0", port=port)
